9f563aa5f1a5c3e48d3704bea203873e056ced1e
BrettFord1999 <92333056+BrettFord1999@users.noreply.github.com>
2022-02-22 12:26:24 -0500

    Update LM-Property-Update.ps1

--- LM-Property-Update.ps1
+++ LM-Property-Update.ps1
@@ -4,7 +4,7 @@
 $key = "********************" |convertto-securestring -AsPlainText -force
 
 #pull data from csv to set to variables
-$csv = import-csv "C:\Users\BFord\OneDrive - Magna5\Book1 Sonoco csv.csv"
+$csv = import-csv "Path to csv file"
 $csv | ForEach-Object {
         $displayname = $_.host
         $location = $_.location
