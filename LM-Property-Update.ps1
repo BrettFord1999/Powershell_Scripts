8d25f25a1f7a77b43dd312ef38e2bb3e8daac81d
BrettFord1999 <92333056+BrettFord1999@users.noreply.github.com>
2022-02-23 10:37:17 -0500

    Update LM-Property-Update.ps1

--- LM-Property-Update.ps1
+++ LM-Property-Update.ps1
@@ -9,5 +9,23 @@ $csv | ForEach-Object {
         $displayname = $_.host
         $location = $_.location
         $prod = $_.category
-        Update-LogicMonitorDeviceProperty -AccessId ************ -AccessKey $key -AccountName magna5global -DisplayName $displayname -PropertyNames in.production,device.location  -PropertyValues $prod,$location
+        Update-LogicMonitorDeviceProperty -AccessId ************ -AccessKey $key -AccountName ******** -DisplayName $displayname -PropertyNames in.production,device.location  -PropertyValues $prod,$location
                       }
+
+###################################################################################################################
+#Create dynamic groups
+
+$csv = Import-Csv "C:\Users\BFord\Downloads\groupsimport.csv"
+$csv | ForEach-Object {
+       $location = $_.location
+       $appliesto = $_.appliesto
+
+
+$table = @{
+    
+    name = "$location"
+            parentId = ""
+            appliesTo = "$appliesto"
+        }
+Add-LogicMonitorDeviceGroup -AccessId ************* -AccessKey $key -AccountName ******** -Properties $table
+}
